08e016da1461c0186097e526982a376b27b79f6a
Daniel Ribas <danielribas1105@gmail.com>
2023-02-16 19:53:43 -0300

    create project sorteador app

---
+++ app/src/main/java/com/apps/sorteadoreasy/activity/HomeActivity.java
@@ -0,0 +1,37 @@
+package com.apps.sorteadoreasy.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.apps.sorteadoreasy.R;
+
+public class HomeActivity extends AppCompatActivity {
+    
+    private TextView numStart, numEnd, numRaffle;
+    private Button btnRaffle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        
+        numStart = findViewById(R.id.textNumberStart);
+        numEnd = findViewById(R.id.textNumberEnd);
+        numRaffle = findViewById(R.id.textNumberRaffle);
+        btnRaffle = findViewById(R.id.btnStartRaffle);
+        
+        btnRaffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(HomeActivity.this, "Iniciar Sorteio", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+    }
+    
+}
