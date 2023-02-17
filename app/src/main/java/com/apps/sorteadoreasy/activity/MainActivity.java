d04c3e2d627a3ac205e0c268dc8834c4e5af93c9
Daniel Ribas <danielribas1105@gmail.com>
2023-02-16 23:55:27 -0300

    app start OK

--- app/src/main/java/com/apps/sorteadoreasy/activity/MainActivity.java
+++ app/src/main/java/com/apps/sorteadoreasy/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.apps.sorteadoreasy.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.apps.sorteadoreasy.R;
 
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startApp (View view){
+        startActivity(new Intent(this, HomeActivity.class));
     }
 }
