bc6a761628032b879cb96e94eddb32c88b25b02d
Daniel Ribas <danielribas1105@gmail.com>
2023-02-17 10:14:39 -0300

    test function random

--- app/src/main/java/com/apps/sorteadoreasy/activity/HomeActivity.java
+++ app/src/main/java/com/apps/sorteadoreasy/activity/HomeActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 import com.apps.sorteadoreasy.R;
 
+import java.util.Random;
+
 public class HomeActivity extends AppCompatActivity {
     
     private TextView numStart, numEnd, numRaffle;
@@ -28,6 +30,8 @@ public class HomeActivity extends AppCompatActivity {
         btnRaffle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int x = new Random().nextInt(11);
+                numRaffle.setText("N" + x);
                 Toast.makeText(HomeActivity.this, "Iniciar Sorteio", Toast.LENGTH_SHORT).show();
             }
         });
